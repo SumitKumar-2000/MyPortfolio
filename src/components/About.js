38e8422513de0bfe6c27815133944a9d3964e449
Sumit Kumar <80626529+SumitKumar-2000@users.noreply.github.com>
2023-06-21 06:44:18 +0530

    Update About.js
    About description updated

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ const About = () => {
         <StyledAbout>
             <div>
                 <h1>About me.</h1>
-                <p>Full Stack Developer from <strong className='country'>INDIA</strong> skilled in HTML, CSS, JavaScript, ReactJs, NodeJs, ExpressJs, MongoDB and many more dev dependencies & technologies. I'm kind of person who is so much curious to learn new technologies, love to interact & teach, love to participate & organize hackathons and tries to complete every task/project in a perfect manner.</p>
+                <p>Full Stack Developer from <strong className='country'>INDIA</strong> with expertise in HTML, CSS, JavaScript, ReactJs, NextJs, NodeJs, ExpressJs, MongoDB, and various other dependencies & technologies. Known for my curiosity and eagerness to learn, I actively engage in interactive teaching, organizing & participating in hackathons, and ensuring the completion of every task and project with precision and excellence.</p>
                 <TransparentButton bg="#0ea5e9"><a href={Resume} download="Resume-SumitKumar">My Resume</a></TransparentButton>
             </div>
             <ImageContainer>
